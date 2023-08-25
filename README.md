39a83f2d6a082c925399b90ff33f12dddaac7921
MehdiHouasse <mehdi.houasse1@gmail.com>
2023-08-25 10:26:38 -0700

    README

---
+++ README.md
@@ -0,0 +1,52 @@
+# Slot Machine Game
+
+A simple browser-based slot machine game where players can place bets, spin the reels, and win prizes based on the symbol combinations.
+
+## Table of Contents
+
+- [Description]
+  ## :computer: Technologies Used
+  ![JavaScript](https://img.shields.io/badge/-JavaScript-05122A?style=flat&logo=javascript)
+  ![HTML5](https://img.shields.io/badge/-HTML5-05122A?style=flat&logo=html5)
+  ![CSS3](https://img.shields.io/badge/-CSS-05122A?style=flat&logo=css3)
+  ![Markdown](https://img.shields.io/badge/-Markdown-05122A?style=flat&logo=markdown)
+  ![Git](https://img.shields.io/badge/-Git-05122A?style=flat&logo=git)
+  ![Github](https://img.shields.io/badge/-GitHub-05122A?style=flat&logo=github)
+  ![VSCode](https://img.shields.io/badge/-VS_Code-05122A?style=flat&logo=visualstudio)
+  ![Materialize CSS](https://img.shields.io/badge/-Materialize_CSS-05122A?style=flat&logo=materialdesign)
+- [Game Features](#game-features)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## ScreenShots
+![initial state](imgs/initial.png)
+![first spin](imgs/spin.png)
+
+
+## Description
+
+The Slot Machine Game is a basic web-based game that simulates a slot machine experience. Players can place bets, spin the reels, and have the chance to win based on predefined symbol combinations. The game is built using JavaScript, HTML, and CSS and provides a straightforward interface for interaction.
+
+## Game Features
+
+- **Place Bets:** Players can input their desired bet amount before spinning the reels.
+- **Spin Reels:** Clicking the "Spin" button initiates the spinning animation of the reels.
+- **Winning Combinations:** The game recognizes winning symbol combinations and calculates the corresponding prizes.
+- **Win Popup:** When a player wins, a popup congratulating them on their victory appears.
+- **Player Balance:** The player's balance is displayed, updating with each bet and win.
+
+## Installation
+
+1. Clone or download this repository to your local machine.
+2. Open the `index.html` file in a web browser to play the game.
+
+## Usage
+
+1. Open the game in a web browser by launching the `index.html` file.
+2. Enter your desired bet amount in the input field and click the "Place Bet" button.
+3. Click the "Spin" button to start the spinning animation of the reels.
+4. The reels will stop at random symbols after a short animation.
+5. If you win, a popup will display the winning amount.
+6. Your balance will be updated according to your wins and bets.
+## getting started
+Click here : https://mehdihouasse.github.io/Slot-Machine-Game/
--- js/main.js
+++ js/main.js
@@ -1,31 +1,68 @@
 /*----- constants -----*/
+//maps symbols to their respective images and ratios
 const SM_LOOKUP = {
   bar: { img: 'imgs/bar.png', two: 1, three: 2 },
-  bell: { img: 'imgs/bell.png', two: 1, three: 4 },
+  bell: { img: 'imgs/bell.png', two: 1, three: 2 },
   cherries: { img: 'imgs/cherries.png', two: 1, three: 2 },
-  lemon: { img: 'imgs/lemon.png', two: 1, three: 3 },
-  seven: { img: 'imgs/seven.png', two: 1, three: 5 },
+  lemon: { img: 'imgs/lemon.png', two: 1, three: 2 },
+  seven: { img: 'imgs/seven.png', two: 1, three: 2 },
 };
-
+// defines the sequence of symbols on the reels
 const SM_RATIO = ['seven', 'bell', 'bell', 'lemon', 'lemon', 'lemon', 'bar', 'bar', 'bar', 'bar', 'cherries', 'cherries', 'cherries', 'cherries', 'cherries'];
 
 /*----- state variables -----*/
+//These variables are essential for keeping track of the game's progres.
 let symbolCounts = {};
 let playerBalance;
 let betAmount;
 let reelPositions;
 let winningAmount;
 let stoppingPoint;
+let outcome;
 
 /*----- cached elements  -----*/
-//const pResultEl = document.getElementById('p-result');
+const winSound = new Audio('sound/congrats.flac');
+const spinAudio = new Audio('sound/sound.mp3');
 const doors = document.querySelectorAll('.door');
 document.querySelectorAll('.door');
+const message = document.querySelector('.popup')
 
 /*----- event listeners -----*/
 document.querySelector('#spinner').addEventListener('click', handleSpin);
 document.querySelector('#reseter').addEventListener('click', init);
 document.querySelector('#place-bet').addEventListener('click', handlePlaceBet);
+document.querySelector('#spinner').addEventListener('click', () => {
+  const spinAudio = new Audio('sound/sound.mp3');
+  spinAudio.addEventListener('ended', () => {
+    spinAudio.pause();
+    spinAudio.currentTime = 0; //
+  });
+  spinAudio.play();
+  setTimeout(() => {
+    spinAudio.pause();
+  }, 3400);
+  handleSpin();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const spinButton = document.getElementById('spinner');
+
+  spinButton.addEventListener('click', () => {
+    // Simulate reel positions for this example
+    const reelPositions = ['bell', 'bell', 'bell'];
+
+    // Calculate winning amount based on reel positions
+    const winningAmount = calculateWinningAmount(reelPositions);
+
+    // Update playerBalance based on winning amount
+    playerBalance += winningAmount;
+
+    // Update balance display
+    const balanceAmountElement = document.getElementById('balance-amount');
+    balanceAmountElement.textContent = playerBalance;
+    render();
+  });
+});
 
 
 /*----- functions -----*/
@@ -33,33 +70,42 @@ document.querySelector('#place-bet').addEventListener('click', handlePlaceBet);
 function init() {
   playerBalance = 100;
   betAmount = 20;
-  reelPositions = ['bell','bell','bell'];
+  reelPositions = ['bell', 'bell', 'bell'];
   winningAmount = 0;
   currentSymbolIndex = 0;
-  //startSpinAnimation();
+  message.style.display = 'none';
   render();
 }
-// function renderResults() {
-//   pResultEl.src = RPS_LOOKUP[results.p].img;
-//   pResultEl.style.borderColor = winner === 'p' ? 'grey' : 'white';
-// }
+//display reel images and manages the outcome of the game
 function render() {
-  // if (reelPositions.length){
-  doors.forEach((value,idx) => {
+  doors.forEach((value, idx) => {
     value.style.backgroundImage = `url(${SM_LOOKUP[reelPositions[idx]].img})`;
-  })
-  // updateReelsAnimation();
-  const outcome = getWinner();
-  console.log(outcome);
-  const winningAmountDisplay = document.getElementById('winning-amount-display');
-  winningAmountDisplay.textContent = `Winning Amount: $${winningAmount}`;
+    value.style.backgroundSize = 'cover';
+  });
 
+  outcome = getWinner();
+  console.log(outcome)
+
+  if (outcome === 'win') {
+    const winningAmountDisplay = document.getElementById('winning-amount-display');
+    winningAmountDisplay.textContent = `Winning Amount: $${winningAmount}`;
+
+    setTimeout(() => {
+      showWinPopup(winningAmount);
+      winningAmount = 0; // Reset the winning amount after showing the popup
+      winningAmountDisplay.textContent = `Winning Amount: $${winningAmount}`;
+    }, 2000); // Adjust the duration as needed
+  } else {
+    const winningAmountDisplay = document.getElementById('winning-amount-display');
+    winningAmountDisplay.textContent = `Winning Amount: $0`;
+  }
 }
+//Handles user bets, updating the bet amount and player balance.
 function handlePlaceBet() {
   const betInput = document.getElementById('bet-amount');
   const userBet = parseInt(betInput.value);
 
-  if (isNaN(userBet) || userBet <= 0 || userBet > playerBalance) {
+  if (userBet <= 0 || userBet > playerBalance) {
     alert("Invalid bet amount. Please enter a valid bet.");
     return;
   }
@@ -68,19 +114,20 @@ function handlePlaceBet() {
   playerBalance -= betAmount;
   render();
 }
+//Initiates the spinning animation of the reels
 function startSpinAnimation() {
   const spinInterval = setInterval(() => {
     updateReelsAnimation();
-  }, 100); // Adjust the interval as needed
+  }, 100); // Adjust the interval
 
   setTimeout(() => {
     clearInterval(spinInterval); // Stop the spinning animation after a certain time
-    //stopReels();
     render();
-  }, 3000); // Adjust the duration as needed
+  }, 3000);
 }
+//Simulates spinning the reels, updating the game's internal state,
+//and triggering the spinning animation.
 function handleSpin() {
-
   symbolCounts = {};
 
   for (let i = 0; i < 3; i++) {
@@ -88,97 +135,44 @@ function handleSpin() {
     reelPositions[i] = SM_RATIO[randomIndex];
     symbolCounts[reelPositions[i]] = (symbolCounts[reelPositions[i]] || 0) + 1;
   }
+
   if (playerBalance < betAmount) {
     alert("Insufficient balance. Please add more credits.");
     return;
   }
 
-  playerBalance -= betAmount;
+  playerBalance -= betAmount*2;
   stoppingPoint = Math.floor(Math.random() * SM_RATIO.length);
   startSpinAnimation();
   updateReelsAnimation();
   setTimeout(() => {
-
-    //stopReels();
+    playerBalance += winningAmount;
+    outcome = getWinner();
+    if (outcome === 'win') {
+      showWinPopup(winningAmount);
+      winningAmount = 0;
+    }
+    render();
   }, 3000);
-  playerBalance += winningAmount;
-  winningAmount = 0;
-
-  render();
 }
 
-//  let currentSymbolIndex = 0; //variable to keep track of the current symbol index
-// function updateReelsAnimation(){
-//   for (){
-
-//   }
-// }
-
+//Updates the reel images during the spinning animation
 function updateReelsAnimation() {
   for (let i = 0; i < doors.length; i++) {
-    const symbolIndex = Math.floor(Math.random()*SM_RATIO.length)
+    const symbolIndex = Math.floor(Math.random() * SM_RATIO.length)
     let symbol = SM_RATIO[symbolIndex]
-    // const symbolContainer = doors[i].querySelector('.spin-container');
     doors[i].style.backgroundImage = `url(${SM_LOOKUP[symbol].img})`;
   }
 
-  // currentSymbolIndex;
 }
-
-
-// function updateReelsAnimation() {
-//   const symbolIndex = currentSymbolIndex % SM_RATIO.length;
-//   console.log(symbolIndex)
-//   doors[0].style.backgroundImage = `url(${SM_LOOKUP[SM_RATIO[symbolIndex]].img})`;
-//   doors[1].style.backgroundImage = `url(${SM_LOOKUP[SM_RATIO[(symbolIndex + 1) % SM_RATIO.length]].img})`;
-//   doors[2].style.backgroundImage = `url(${SM_LOOKUP[SM_RATIO[(symbolIndex + 2) % SM_RATIO.length]].img})`;
-
-//   currentSymbolIndex++;
-
-//   // if (currentSymbolIndex < SM_RATIO.length) {
-//   //   requestAnimationFrame(updateReelsAnimation); //
-//   // } else {
-//   //   animationComplete();
-//   // }
-// }
-
-
-
-
-
 function animationComplete() {
   stopReels();
   // Call render when the animation stops
 }
 
-
-
-
-// function stopReels() {
-//   console.log(doors);
-//   doors.forEach(reel => {
-//     const symbolContainers = reel.querySelectorAll('.doors');
-//     // console.log(symbolContainers);
-//     symbolContainers.forEach(container => {
-//       container.classList.toggle('spin-container', false);
-//     });
-
-//   });
-//   console.log(reelPositions);
-//   doors[0].style.backgroundImage = `url(${SM_LOOKUP[reelPositions[0]].img})`;
-//   doors[1].style.backgroundImage = `url(${SM_LOOKUP[reelPositions[1]].img})`
-//   doors[2].style.backgroundImage = `url(${SM_LOOKUP[reelPositions[2]].img})`
-//    render();
-// }
-
+//Determines the winning outcome based on symbol combinations.
 function getWinner() {
-  // const symbolCounts = {};
 
-  // for (let i = 0; i < 3; i++) {
-  //   const randomIndex = Math.floor(Math.random() * SM_RATIO.length);
-  //   reelPositions[i] = SM_RATIO[randomIndex];
-  //   symbolCounts[reelPositions[i]] = (symbolCounts[reelPositions[i]] || 0) + 1;
-  // }
 
   const winningCombinations = [
     { symbols: ['bar', 'bar', 'bar'], outcome: 'win' },
@@ -192,17 +186,36 @@ function getWinner() {
   const winningCombination = winningCombinations.find(combination => {
     return combination.symbols.every(symbol => symbolCounts[symbol] === 3);
   });
-console.log(winningCombination);
+
   winningAmount = winningCombination ? calculateWinningAmount() : 0;
   return winningCombination ? 'win' : 'lose';
 }
-
+//Calculates the winning amount based on symbol occurrences and payout ratios.
 function calculateWinningAmount() {
   const symbolsInWinningCombination = reelPositions[0];
   const occurrenceCount = reelPositions.filter(symbol => symbol === symbolsInWinningCombination).length;
   const payoutMultiplier = SM_LOOKUP[symbolsInWinningCombination].three;
+  console.log('amount', betAmount * payoutMultiplier * occurrenceCount)
   return betAmount * payoutMultiplier * occurrenceCount;
 }
+//Displays a winning message with the winning amount,
+ //and hides it after 5 sec.
+function showWinPopup(winningAmount) {
+  console.log('amount', winningAmount);
+  if (winningAmount > 0) {
+    message.style.display = 'block';
+    message.textContent = `Congratulations! You won $${winningAmount}!`;
+    message.classList.add('win-message');
+    winSound.play();
+    setTimeout(() => {
+      message.classList.remove('win-message');
+      message.style.display = 'none'; // Hide the popup after a certain duration
+    }, 5000);
+  }
+}
+
+
+
 
 // Initialize the game
 init();
